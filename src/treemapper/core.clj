3f74855f268d0e857cb1fa933e4926a70e3c7298
Robert Boyd <robertb@groupon.com>
2013-03-08 23:12:51 -0600

    Adding ability to filter files by regex during analyze step.

--- src/treemapper/core.clj
+++ src/treemapper/core.clj
@@ -10,11 +10,17 @@
         (string? s) (File. s)  ; return java.io.File for path s
         :else (throw (FileNotFoundException. (str s)))))
 
-(defn walk [^File file]
+(defn filter-files [^File file ignore]
+  (let [files        (.listFiles file)
+        regex-filter (fn [fname]
+                       (not-any? #(re-find % (.getName fname)) ignore))]
+    (filter regex-filter files)))
+
+(defn walk [^File file & {:keys [ignore]}]
   (let [node     {:name (.getName file)}
-        children (map walk (.listFiles file))]
+        children (map walk (filter-files file ignore))]
     (-> (if (empty? children) (assoc node :size (.length file)) (assoc node :children children))
         (assoc :modified (.lastModified file)))))
 
-(defn analyze [dir]
-  (spit "output.json" (-> dir as-file walk json/write-str)))
+(defn analyze [dir & {:keys [ignore]}]
+  (spit "output.json" (-> dir as-file (walk :ignore ignore) json/write-str)))
